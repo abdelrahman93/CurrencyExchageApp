3585f69ca8b2c5e79504518f01f3f2691aadf5b3
abdelrahman93 <a.shreef93@gmail.com>
2023-09-16 20:54:43 +0300

    US-1 (Calculation of Currency Exchange )
    *Exchange Currency Calculation
    *Swap functionality between currencies
    
    Signed-off-by: abdelrahman93 <a.shreef93@gmail.com>

--- app/src/main/java/com/task/currencyapp/ui/currencyexchange/CurrencyExchangeFragment.kt
+++ app/src/main/java/com/task/currencyapp/ui/currencyexchange/CurrencyExchangeFragment.kt
@@ -18,6 +18,10 @@ import kotlinx.android.synthetic.main.fragment_exchange_currency.*
 class CurrencyExchangeFragment :
     BaseFragment<CurrencyExchangeViewModel>(CurrencyExchangeViewModel::class.java) {
 
+    private var exchangeFromRate = 0.0
+    private var exchangeToRate = 0.0
+    private var convertCurrencyFrom = true
+
     override fun injectDagger() = DaggerAppComponent.factory()
         .create(requireContext())
         .inject(this)
@@ -25,7 +29,13 @@ class CurrencyExchangeFragment :
     override fun getLayout() = R.layout.fragment_exchange_currency
 
     override fun initView() {
-        etValueFrom.addTextChangedListener(textWatcher)
+        etValueFrom.addTextChangedListener(textWatcherFrom)
+        etValueTo.addTextChangedListener(textWatcherTo)
+
+        btnSwap.setOnClickListener {
+            swapButtonClicked()
+        }
+
         viewModel.getCurrencyRates()
     }
 
@@ -50,24 +60,14 @@ class CurrencyExchangeFragment :
         findNavController().navigate(action)
     }
 
-    private val textWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            // pass
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            btnDetails.isEnabled = etValueFrom.text?.isNotEmpty() == true
-        }
-
-        override fun afterTextChanged(s: Editable?) {
-        }
-    }
-
     private fun setupSpinnerFromAndTo(rates: Rates) {
         val ratesArray = viewModel.convertRatesToArrayList(rates)
         val adapter = activity?.let { CustomSpinnerAdapter(it, ratesArray) }
         spFromCurrencies.adapter = adapter
         spToCurrencies.adapter = adapter
+
+        // Default value to set EGP
+        spToCurrencies.setSelection(3)
 
         spFromCurrencies.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -77,16 +77,91 @@ class CurrencyExchangeFragment :
                 id: Long
             ) {
                 // Get the selected Rate object
-                val selectedRate = ratesArray[position]
-
+                val selectedFromRate = ratesArray[position]
                 // Handle the selected rate here
-                val currencyCode = selectedRate.currencyCode
-                val exchangeRate = selectedRate.exchangeRate
+                exchangeFromRate = selectedFromRate.exchangeRate
+                setupValueTo()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 // Handle the case where nothing is selected (optional)
             }
         }
+
+        spToCurrencies.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Get the selected Rate object
+                val selectedToRate = ratesArray[position]
+                // Handle the selected rate here
+                exchangeToRate = selectedToRate.exchangeRate
+                setupValueTo()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // Handle the case where nothing is selected (optional)
+            }
+        }
+    }
+
+    fun setupValueTo() {
+        //From Endpoint Base is EUR to All Currencies rate
+        //Equation = (rateCurrencyFrom / rateCurrencyTo) * numbers of [CurrencyFrom]
+        val fromFieldNumbers = etValueFrom.text.toString().toDouble()
+        val result = (exchangeToRate / exchangeFromRate) * fromFieldNumbers
+        val formattedValue = String.format("%.2f", result)
+        etValueTo.setText(formattedValue)
+    }
+
+    fun setupValueFrom() {
+        val toFieldNumbers = etValueTo.text.toString().toDouble()
+        val result = (exchangeFromRate / exchangeToRate) * toFieldNumbers
+        val formattedValue = String.format("%.2f", result)
+        etValueFrom.setText(formattedValue)
+    }
+
+    private val textWatcherFrom = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // pass
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            convertCurrencyFrom = etValueFrom.isInputMethodTarget
+            btnDetails.isEnabled =
+                (etValueFrom.text?.isNotEmpty() == true) && (etValueTo.text?.isNotEmpty() == true)
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            if (convertCurrencyFrom) {
+                setupValueTo()
+            }
+        }
+    }
+
+    private val textWatcherTo = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // pass
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            convertCurrencyFrom = !etValueTo.isInputMethodTarget
+            btnDetails.isEnabled =
+                (etValueFrom.text?.isNotEmpty() == true) && (etValueTo.text?.isNotEmpty() == true)
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            if (!convertCurrencyFrom) {
+                setupValueFrom()
+            }
+        }
+    }
+
+    private fun swapButtonClicked() {
+        spFromCurrencies.setSelection(spFromCurrencies.selectedItemPosition)
+        spToCurrencies.setSelection(spToCurrencies.selectedItemPosition)
     }
 }
